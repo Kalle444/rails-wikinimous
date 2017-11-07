17cb628d7f8acf3be13dedcc6d153a8dfc06249c
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-07 17:04:05 +0100

    app working and kramdown implemented

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :update, :destroy, :edit]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    params[:article][:content] = kramdown(params[:article][:content])
+    Article.create(article_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def kramdown(text)
+  return Kramdown::Document.new(text).to_html
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+require 'faker'
+require 'kramdown'
+
+10.times do
+  Article.create(
+    title: Faker::SiliconValley.company,
+    content: Faker::Lorem.sentence(100)
+    )
+end
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
